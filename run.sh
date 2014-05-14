0690746781d72e9f8b7ba75028d0d301ad5bac83
--global <--global>
2014-05-14 06:49:46 +0000

    Changed fan to analog out Added second endstop to gantry

--- run.sh
+++ run.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
+set -x
+
+# Rip environement
+#. ~/machinekit/scripts/rip-environment
+
 export DEBUG=5
 
 # make msgd builtin webserver to respond on 192.168.7.1
-#export MSGD_OPTS="--wsdebug 255 --wwwdir /home/linuxcnc/machinekit/src/rtapi/www"
-export MSGD_OPTS="--wsdebug 15 --wwwdir /home/linuxcnc/machinekit/src/rtapi/www"
+#export MSGD_OPTS="--wsdebug 255 --wwwdir /home/machinekit/machinekit/src/rtapi/www"
+export MSGD_OPTS="--wsdebug 15 --wwwdir $EMC2_HOME/src/rtapi/www"
 
 # if halscope should connect via X somewhere else, say so here:
 #export DISPLAY=1.2.3.4:0.0
 #mah: export DISPLAY=193.228.47.195:0.0
 
-. /home/linuxcnc/machinekit/scripts/rip-environment
-
 # kill any current sessions
 realtime stop
-
-#cd  /home/linuxcnc/machinekit/src/hal/haltalk/examples
 
 # this will leave a halcmd prompt - to check status etc
 # to exit the demo just hit ^D
 
-python /home/linuxcnc/machinekit/src/middleware/config-service/configserver.py &
+python $EMC2_HOME/src/machinetalk/config-service/configserver.py apps.ini &
+CONFIG_SERVER_PID=$!
 linuxcnc TCT3D.ini
+
+# cleanup
+kill $CONFIG_SERVER_PID
 
 exit 0
 
